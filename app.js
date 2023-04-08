5007ded912dde64d11c348bde464d8763652fb55
Daviid <mdavidcha@gmail.com>
2023-04-09 01:45:59 +0200

    Mi primer punto de partidMi primer punto de partidaa

---
+++ app.js
@@ -0,0 +1,13 @@
+/*function imprimir(){
+    for( var i =0; i< 8000; i++ ){
+        console.log(imprimir);
+    }
+}
+
+function precionoClick(){*/
+    console.log("Bienvenida angie"); /*
+}
+
+
+imprimir();
+
